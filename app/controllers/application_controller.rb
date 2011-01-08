8a19a97209433a75ef013df4a47c4333a7a3733b
Osledy Bazo <osledybazo@gmail.com>
2011-01-08 15:26:01 -0430

    reforming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,17 @@ class ApplicationController < ActionController::Base
   
   def index
     page = params[:page] || 1
-    @live_now = Event.find( :all, :order=>"date desc", :limit=>"3", :conditions => ["date > ? and date < ?", (DateTime.now - 10.minutes), DateTime.now ] )
-    @upcomming_events = Event.find( :all, :order=>"date desc", :limit=>"3", :conditions => ["date > ?", DateTime.now ] )
+    
+    @live_now = Event.find( :all, 
+                            :order=>"date desc", 
+                            :limit=>"3", 
+                            :conditions => ["date > ? and date < ?", (DateTime.now - 10.minutes), (DateTime.now + 10.minutes) ] )
+    
+    @upcomming_events = Event.find( :all, 
+                                    :order=>"date desc", 
+                                    :limit=>"3", 
+                                    :conditions => ["date > ? and date < ?", DateTime.now, (DateTime.now + 6.hours) ] )
+    
     @latest_submited_events = Event.paginate( :all, :order=>"updated_at desc", :page=> page )
   end
 end
